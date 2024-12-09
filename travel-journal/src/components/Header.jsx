89bf60054b3d8494879dfdc9047a4dc62ef07c80
Anuska86 <anasrm86@gmail.com>
2024-12-09 16:25:10 +0100

    main

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <img />
+                <span><h2>My travel jornal</h2></span>
+            </nav>
+        </header>
+    )
+}
