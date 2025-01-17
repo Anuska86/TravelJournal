07cfb82cc36489f7cf3bf70c09a27091b1148561
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-17 12:27:32 +0100

    Data

---
+++ travel-journal/src/data.js
@@ -0,0 +1,38 @@
+export default[
+{
+    id:1,
+    img_properties:"Picture of a beautiful japaness temple",
+    img:"./imgs/japan.png",
+    title:"Temple of Japan",
+    country:"Japan",
+    googleMapsLink:"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
+    date:"22 March, 2024 - 7 April,2024",
+    text:"In Japan, two kinds of religious buildings can be found: Buddhist temples (寺) and Shinto shrines (神社). They share some common features and are often confused with one another. This is because they both feature traditional Japanese architecture: usually wooden structures with columns and typical large roofs. The influences of both religions are mixed as much on the substance as on the form. Practices and rituals cross and it is not uncommon to find Buddha symbols in a Shinto shrine and vice versa. This is due to the religious syncretism that has persisted in Japan since the introduction of Buddhism around the 5th century. In the Land of the Rising Sun there are more than 100,000 Japanese shrines, spread throughout the country. As for the number of temples, they are estimated to be around 80,000."
+},
+
+{id:,
+    img_properties:,
+    img:,
+    title:,
+    country:"",
+    googleMapsLink:"",
+    date:"",
+    text:""},
+    {id:,
+        img_properties:,
+        img:,
+        title:,
+        country:"",
+        googleMapsLink:"",
+        date:"",
+        text:""},
+        {id:,
+            img_properties:,
+            img:,
+            title:,
+            country:"",
+            googleMapsLink:"",
+            date:"",
+            text:""}
+
+]
