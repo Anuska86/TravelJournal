fdbe28b28ee31b6895295e159c28944bb26f79a1
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-18 13:21:42 +0100

    style

--- travel-journal/src/components/Footer.jsx
+++ travel-journal/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 export default function Footer() {
   return (
     <footer>
-      <span className="footerItems"></span>
-      <h3>Author: Ana Sappia Rey</h3>
+      <nav>
+        <span className="footerItems"></span>
+        <span>
+          <h4 className="footerLetters">2024 - Author: Ana Sappia Rey</h4>
+        </span>
+      </nav>
     </footer>
   );
 }
